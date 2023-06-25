2659b8198ebc31aa3b6c830358ff87fe95a7db04
Yuto Takakura <yutotakakura0320@gmail.com>
2023-06-25 19:49:14 +0900

    Add public endpoint with pagination and tests

---
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Travel;
+use App\Http\Resources\TravelResource;
+
+class TravelController extends Controller
+{
+    public function index()
+    {
+        $travels = Travel::where('is_public', true)->paginate();
+        return TravelResource::collection($travels);
+    }
+}
