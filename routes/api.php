8a2e90b80a8591d35b025c64daf31943d2e48403
Yuto Takakura <yutotakakura0320@gmail.com>
2023-07-01 12:19:53 +0900

    Add public endpoint for tours with tests

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TravelController;
+use App\Http\Controllers\Api\V1\TourController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('travels', [TravelController::class, 'index']);
+Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
